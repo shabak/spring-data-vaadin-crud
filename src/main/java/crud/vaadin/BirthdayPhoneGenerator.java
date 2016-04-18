da5a16205c1a4846bf86fdc1094a4b26c6d1d479
Nikolay Shabak <n.shabak@mretailer.ru>
2016-04-18 21:03:00 +0300

    GeneratedPropertyListContainer test usecase implemented

---
+++ src/main/java/crud/vaadin/BirthdayPhoneGenerator.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2016, i-Free. All Rights Reserved.
+ * Use is subject to license terms.
+ */
+
+package crud.vaadin;
+
+import com.vaadin.data.Item;
+import com.vaadin.data.util.PropertyValueGenerator;
+import crud.backend.Person;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * StatusGenerator - TODO: description
+ *
+ * @author Nikolay Shabak (nikolay)
+ * @since 12.04.2016
+ */
+public class BirthdayPhoneGenerator extends PropertyValueGenerator<String> {
+
+    SimpleDateFormat dateFormat = new SimpleDateFormat("MM-dd-yyyy");
+    @Override
+    public String getValue(Item item, Object itemId, Object propertyId) {
+        Person p = (Person)itemId;
+        Date birthDay = p.getBirthDay();
+        StringBuilder displayValue = new StringBuilder();
+        if (birthDay != null) {
+            displayValue.append("BD: ");
+            displayValue.append(dateFormat.format(birthDay));
+        }
+        String phoneNumber = p.getPhoneNumber();
+        if (phoneNumber != null) {
+            if (displayValue.length() > 0) {
+                displayValue.append("; ");
+            }
+            displayValue.append(" Phone: ");
+            displayValue.append(phoneNumber);
+        }
+        return displayValue.toString();
+    }
+
+    @Override
+    public Class<String> getType() {
+        return String.class;
+    }
+}
--- src/main/java/crud/vaadin/MainUI.java
+++ src/main/java/crud/vaadin/MainUI.java
@@ -8,15 +8,16 @@ import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.UI;
+import com.vaadin.ui.renderers.HtmlRenderer;
 import crud.backend.Person;
 import crud.backend.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
-import org.vaadin.viritin.SortableLazyList;
+import org.vaadin.viritin.GeneratedPropertyListContainer;
 import org.vaadin.viritin.button.ConfirmButton;
 import org.vaadin.viritin.button.MButton;
-import org.vaadin.viritin.fields.MTable;
+import org.vaadin.viritin.grid.MGrid;
 import org.vaadin.viritin.label.RichText;
 import org.vaadin.viritin.layouts.MHorizontalLayout;
 import org.vaadin.viritin.layouts.MVerticalLayout;
@@ -29,24 +30,27 @@ import org.vaadin.viritin.layouts.MVerticalLayout;
 @SpringUI
 public class MainUI extends UI {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@Autowired
+    @Autowired
     PersonRepository repo;
 
-    private MTable<Person> list = new MTable<>(Person.class)
-            .withProperties("id", "name", "email")
-            .withColumnHeaders("id", "Name", "Email")
-            .setSortableProperties("name", "email")
-            .withFullWidth();
+    private static final String ID = "id";
+    private static final String NAME = "name";
+    private static final String EMAIL = "email";
+    private static final String BIRTHDAY = "birthDay";
+    private static final String PHONE = "phone";
+    private static final String BIRTHDAY_PHONE = "birthday_phone";
+    String[] columns = {ID, NAME, EMAIL, BIRTHDAY_PHONE};
+
+    private MGrid<Person> grid = new MGrid<>();
 
     private Button addNew = new MButton(FontAwesome.PLUS, this::add);
     private Button edit = new MButton(FontAwesome.PENCIL_SQUARE_O, this::edit);
     private Button delete = new ConfirmButton(FontAwesome.TRASH_O,
             "Are you sure you want to delete the entry?", this::remove);
+
+    private BirthdayPhoneGenerator birthdayPhoneGenerator = new BirthdayPhoneGenerator();
 
     @Override
     protected void init(VaadinRequest request) {
@@ -54,32 +58,34 @@ public class MainUI extends UI {
                 new MVerticalLayout(
                         new RichText().withMarkDownResource("/welcome.md"),
                         new MHorizontalLayout(addNew, edit, delete),
-                        list
-                ).expand(list)
+                        grid
+                ).expand(grid)
         );
+
+        GeneratedPropertyListContainer<Person> container = new GeneratedPropertyListContainer(Person.class, columns);
+        container.addGeneratedProperty(BIRTHDAY_PHONE, birthdayPhoneGenerator);
+        grid.setContainerDataSource(container);
+
+        grid.getColumn(ID).setHeaderCaption("ID");
+        grid.getColumn(NAME).setHeaderCaption("Name");
+        grid.getColumn(EMAIL).setHeaderCaption("Email");
+        grid.getColumn(BIRTHDAY_PHONE).setHeaderCaption("Details").setRenderer(new HtmlRenderer());
+
+        grid.addSelectionListener( event -> onSelectionChanged() );
+        grid.setSizeFull();
         listEntities();
-        list.addMValueChangeListener(e -> adjustActionButtonState());
     }
 
-    protected void adjustActionButtonState() {
-        boolean hasSelection = list.getValue() != null;
-        edit.setEnabled(hasSelection);
-        delete.setEnabled(hasSelection);
+    private void onSelectionChanged() {
+        boolean oneRowSelected = grid.getSelectedRows().size() == 1;
+        edit.setEnabled(oneRowSelected);
+        delete.setEnabled(oneRowSelected);
     }
 
     static final int PAGESIZE = 45;
 
     private void listEntities() {
-        // A dead simple in memory listing would be:
-        // list.setBeans(repo.findAll());
-
-        // Lazy binding with SortableLazyList: memory and query efficient 
-        // connection from Vaadin Table to Spring Repository
-        // Note that fetching strategies can be given to MTable constructor as well.
-        // Use this approach if you expect you'll have lots of data in your 
-        // table.
-        
-        list.setBeans(new SortableLazyList<>(
+        grid.lazyLoadFrom(
                 // entity fetching strategy
                 (firstRow, asc, sortProperty) -> repo.findAllBy(
                         new PageRequest(
@@ -93,8 +99,8 @@ public class MainUI extends UI {
                 // count fetching strategy
                 () -> (int) repo.count(),
                 PAGESIZE
-        ));
-        adjustActionButtonState();
+        );
+        onSelectionChanged();
 
     }
 
@@ -103,18 +109,16 @@ public class MainUI extends UI {
     }
 
     public void edit(ClickEvent e) {
-        edit(list.getValue());
+        edit(grid.getSelectedRow());
     }
 
     public void remove(ClickEvent e) {
-        repo.delete(list.getValue());
-        list.setValue(null);
+        repo.delete(grid.getSelectedRow());
         listEntities();
     }
 
     protected void edit(final Person phoneBookEntry) {
-        PhoneBookEntryForm phoneBookEntryForm = new PhoneBookEntryForm(
-                phoneBookEntry);
+        PhoneBookEntryForm phoneBookEntryForm = new PhoneBookEntryForm(phoneBookEntry);
         phoneBookEntryForm.openInModalPopup();
         phoneBookEntryForm.setSavedHandler(this::saveEntry);
         phoneBookEntryForm.setResetHandler(this::resetEntry);
